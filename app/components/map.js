1b3d66c86b0af8d4f380edf5b5f8d3c8ddd58436
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2017-07-06 18:59:11 +0300

    add map

--- app/components/map.js
+++ app/components/map.js
@@ -1,3 +1,22 @@
 import React from 'react';
+import Panel from 'react-bootstrap/lib/Panel';
+import Map from '2gis-maps-react/lib/Map';
+import Marker from '2gis-maps-react/lib/Marker';
 
-export default ()=><h1>Map page</h1>
+const myPos = [46.4819845,30.7346978];
+
+export default props => {
+
+    return(
+        //<Panel header={<h3>Map</h3>}>
+            <Map
+                style={{width: "100%", height: "500px"}}
+                center={myPos}
+                zoom={16}
+            >
+                <Marker pos={myPos} staticLabel="It's my location" />
+            </Map>
+        //</Panel>
+    );
+
+}
