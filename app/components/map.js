e0f0719a4eae13d361fffca0e739ac29f0bd8587
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2017-07-06 22:24:40 +0300

    add zoom buttons

--- app/components/map.js
+++ app/components/map.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import Panel from 'react-bootstrap/lib/Panel';
+import { connect } from 'react-redux';
+import { zoomIn, zoomOut, setZoom } from '../reducers/map';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
+import Button from 'react-bootstrap/lib/Button';
 import Map from '2gis-maps-react/lib/Map';
 import Marker from '2gis-maps-react/lib/Marker';
 
 const myPos = [46.4819845,30.7346978];
 
-export default props => {
+const MapComponent = props => {
+    const { zoom } = props;
 
     return(
-        //<Panel header={<h3>Map</h3>}>
+        <div>
+            <ButtonGroup>
+                <Button onClick = {props.zoomIn}>Zoom In</Button>
+                <Button onClick = {props.zoomOut}>Zoom Out</Button>
+            </ButtonGroup>
             <Map
                 style={{width: "100%", height: "500px"}}
                 center={myPos}
-                zoom={16}
+                zoom={zoom}
+                onZoomend = { e => props.setZoom(e.target.getZoom()) }
             >
                 <Marker pos={myPos} staticLabel="It's my location" />
             </Map>
-        //</Panel>
+        </div>
     );
 
-}
+};
+
+export default connect(
+    state => ({
+        zoom: state.map.zoom
+    }),
+    { zoomIn, zoomOut, setZoom }
+)(MapComponent);
--- app/reducers/map.js
+++ app/reducers/map.js
@@ -1,2 +1,25 @@
 import { combineReducers } from 'redux';
 
+const ZOOM_IN = 'map/ZOOM_IN';
+const ZOOM_OUT = 'map/ZOOM_OUT';
+const SET_ZOOM = 'map/SET_ZOOM';
+
+const zoom = (state = 16, action = {}) => {
+    switch (action.type){
+        case ZOOM_IN:
+            return state < 20 ? state + 1 : state;
+        case ZOOM_OUT:
+            return state > 0 ? state - 1 : state;
+        case SET_ZOOM:
+            return action.zoom;
+        default: return state;
+    }
+};
+
+export default combineReducers({
+    zoom
+})
+
+export const zoomIn = ()=> ({type: ZOOM_IN});
+export const zoomOut = ()=> ({type: ZOOM_OUT});
+export const setZoom = zoom => ({type: SET_ZOOM, zoom});
