719fb4ecd866a1950e01cdedeb5722c8922ce086
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2017-07-08 12:54:22 +0300

    enable-disable add markers

--- app/components/map.js
+++ app/components/map.js
@@ -3,37 +3,75 @@ import { connect } from 'react-redux';
 import { zoomIn, zoomOut, setZoom, addMarker, undoAddMarker, saveMarkers, fetchMarkers } from '../reducers/map';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Button from 'react-bootstrap/lib/Button';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Row from 'react-bootstrap/lib/Row';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import Checkbox from 'react-bootstrap/lib/Checkbox';
 import Map from '2gis-maps-react/lib/Map';
 import Marker from '2gis-maps-react/lib/Marker';
 
 const myPos = [46.4819845,30.7346978];
 
-const MapComponent = props => {
-    const { zoom, markers } = props;
+class MapComponent extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            enableAddMarkers: false
+        }
+    }
 
-    return(
-        <div>
-            <ButtonGroup>
-                <Button onClick = {props.zoomIn}>Zoom In</Button>
-                <Button onClick = {props.zoomOut}>Zoom Out</Button>
-                <Button onClick = {props.undoAddMarker}>Undo add marker</Button>
-                <Button onClick = {props.saveMarkers}>Save</Button>
-                <Button onClick = {props.fetchMarkers}>Load</Button>
-            </ButtonGroup>
-            <Map
-                style={{width: "100%", height: "500px"}}
-                center={myPos}
-                zoom={zoom}
-                onZoomend = { e => props.setZoom(e.target.getZoom()) }
-                onClick = { e => {props.addMarker(e.latlng)} }
-            >
-                <Marker pos={myPos} staticLabel="It's my location" />
-                { markers.map( (marker, index) => (
-                    <Marker pos = {marker.pos} key = {index} />
-                )) }
-            </Map>
-        </div>
-    );
+    clickMapHandler(e){
+        if( this.state.enableAddMarkers ){
+            this.props.addMarker(e.latlng)
+        } else {
+            //TODO show near objects
+        }
+
+    }
+
+    render() {
+        const props = this.props;
+        const {zoom, markers} = props;
+
+        return (
+            <div className="pageMap">
+                <Navbar.Form>
+                    <Row>
+                        <ButtonGroup>
+                            <Button onClick={props.zoomIn}>Zoom In</Button>
+                            <Button onClick={props.zoomOut}>Zoom Out</Button>
+                            <Button onClick={props.undoAddMarker}>Undo add marker</Button>
+                            <Button onClick={props.saveMarkers}>Save</Button>
+                            <Button onClick={props.fetchMarkers}>Load</Button>
+                        </ButtonGroup>
+                    </Row>
+                </Navbar.Form>
+                <form>
+                    <Checkbox
+                        checked={this.state.enableAddMarkers}
+                        onClick = {
+                            ()=>this.setState( {enableAddMarkers: ! this.state.enableAddMarkers} )
+                        }>
+                        Enable add markers by click
+                    </Checkbox>
+                </form>
+                <Map
+                    style={{width: "100%", height: "500px"}}
+                    center={myPos}
+                    zoom={zoom}
+                    onZoomend={ e => props.setZoom(e.target.getZoom()) }
+                    onClick={ ::this.clickMapHandler }
+                >
+                    <Marker pos={myPos} staticLabel="It's my location"/>
+                    { markers.map((marker, index) => (
+                        <Marker pos={marker.pos} key={index}/>
+                    )) }
+                </Map>
+            </div>
+        );
+    }
 
 };
 
